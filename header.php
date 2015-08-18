1f3aba0be79c20c0e30d67b69ec0295a08ae499e
Jon Winton <winton.jon@gmail.com>
2015-08-18 13:28:37 -0400

    Adding favicon and apple touch icons

--- header.php
+++ header.php
@@ -16,6 +16,19 @@
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
+<!-- Favicon & Apple Touch Icons -->
+<link rel="shortcut icon" href="<?php print get_stylesheet_directory_uri(); ?>/assets/images/favicon.ico" />
+<link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+<link rel="apple-touch-icon" href="<?php print get_stylesheet_directory_uri(); ?>/assets/images/apple-touch-icon.png" />
+<link rel="apple-touch-icon" sizes="57x57" href="<?php print get_stylesheet_directory_uri(); ?>/assets/images/apple-touch-icon-57x57.png" />
+<link rel="apple-touch-icon" sizes="72x72" href="<?php print get_stylesheet_directory_uri(); ?>/assets/images/apple-touch-icon-72x72.png" />
+<link rel="apple-touch-icon" sizes="76x76" href="<?php print get_stylesheet_directory_uri(); ?>/assets/images/apple-touch-icon-76x76.png" />
+<link rel="apple-touch-icon" sizes="114x114" href="<?php print get_stylesheet_directory_uri(); ?>/assets/images/apple-touch-icon-114x114.png" />
+<link rel="apple-touch-icon" sizes="120x120" href="<?php print get_stylesheet_directory_uri(); ?>/assets/images/apple-touch-icon-120x120.png" />
+<link rel="apple-touch-icon" sizes="144x144" href="<?php print get_stylesheet_directory_uri(); ?>/assets/images/apple-touch-icon-144x144.png" />
+<link rel="apple-touch-icon" sizes="152x152" href="<?php print get_stylesheet_directory_uri(); ?>/assets/images/apple-touch-icon-152x152.png" />
+<link rel="apple-touch-icon" sizes="180x180" href="<?php print get_stylesheet_directory_uri(); ?>/assets/images/apple-touch-icon-180x180.png" />
+
 <!-- Twitter META Tags -->
 <meta name="twitter:card" content="summary">
 <meta name="twitter:url" content="<?php the_permalink(); ?>">
@@ -27,7 +40,7 @@
 <meta property="og:title" content="<?php bloginfo( 'name' ); ?> | <?php the_title(); ?>"/>
 <meta property="og:url" content="<?php the_permalink(); ?>"/>
 <meta property="og:site_name" content="<?php bloginfo( 'name' ); ?>"/>
-<!-- <meta property="og:image" content=""/> -->
+<meta property="og:image" content="<?php print get_stylesheet_directory_uri(); ?>/assets/images/hmlogo.png"/>
 <meta property="og:type" content="blog"/>
 <meta property="og:description" content="Hayley Munguia is a data reporter and social media editor at FiveThirtyEight."/>
 <!-- /OG Tags -->
